c735de3050a430d0cf40031ce9ad11e637ed5522
SupAriNa <tyu22528@gmail.com>
2025-11-11 22:41:54 -0500

    remove roster var

--- roster.py
+++ roster.py
@@ -1,13 +1,9 @@
 # https://goheels.com/sports/mens-basketball/roster/2023-24
 import pandas as pd
 
-roster_last = ['High', 'Cadeau', 'Trimble', 'Ingram', 'Ryan', 'Davis', 'Bacot', 'Wojcik', 'Washington', 'Lebo']
-roster_first = ['Zayden', 'Elliot', 'Seth', 'Harrison', 'Cormac', 'RJ', 'Armando', 'Paxson', 'Jalen', 'Creighton']
-height = [81, 73, 75, 79, 77, 72, 83, 77, 82, 73]
-weight = [225, 180, 195, 225, 195, 180, 240, 195, 230, 180]
-player = {"Last Name": roster_last,
-          "First Name": roster_first,
-          "height": height,
-          "weight": weight}
+player = {"Last Name": ['High', 'Cadeau', 'Trimble', 'Ingram', 'Ryan', 'Davis', 'Bacot', 'Wojcik', 'Washington', 'Lebo'],
+          "First Name": ['Zayden', 'Elliot', 'Seth', 'Harrison', 'Cormac', 'RJ', 'Armando', 'Paxson', 'Jalen', 'Creighton'],
+          "height": [81, 73, 75, 79, 77, 72, 83, 77, 82, 73],
+          "weight": [225, 180, 195, 225, 195, 180, 240, 195, 230, 180]}
 data = pd.DataFrame(player)
 print(data)
